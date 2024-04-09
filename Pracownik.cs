f037ab7dd5a75750c79c553c39c6c102765378c0
Wojciech Pietrzak <118462265+Usadowiony@users.noreply.github.com>
2024-04-09 14:30:45 +0200

    Add files via upload

---
+++ Pracownik.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CWzLinq
+{
+    public class Pracownik
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int IloscLatPrzepracowanych { get; set; }
+        public int Wiek { get; set; }
+        public string StopienNaukowy { get; set; }
+        public string Stanowisko { get; set; }
+        public decimal WynagrodzenieMiesieczne { get; set; }
+        public int IloscUrlopuDoPobrania { get; set; }
+
+
+
+        public static void Print(IEnumerable<Pracownik> pracownicy)
+        {
+            foreach (var pracownik in pracownicy)
+            {
+                Console.WriteLine($"Imię: {pracownik.Imie}, Nazwisko: {pracownik.Nazwisko}, Ilość lat przepracowanych: {pracownik.IloscLatPrzepracowanych}, Wiek: {pracownik.Wiek}, Stopień naukowy: {pracownik.StopienNaukowy}, Stanowisko: {pracownik.Stanowisko}, Wynagrodzenie miesięczne: {pracownik.WynagrodzenieMiesieczne}, Ilość urlopu do pobrania: {pracownik.IloscUrlopuDoPobrania}");
+            }
+        }
+
+
+
+        public static List<Pracownik> GenerujPracownikow()
+        {
+            List<Pracownik> pracownicy = new List<Pracownik>()
+         {
+             new Pracownik { Imie = "Jan", Nazwisko = "Kowalski", IloscLatPrzepracowanych = 5, Wiek = 35, StopienNaukowy = "Magister", Stanowisko = "Analityk", WynagrodzenieMiesieczne = 5000, IloscUrlopuDoPobrania = 20 },
+             new Pracownik { Imie = "Anna", Nazwisko = "Nowak", IloscLatPrzepracowanych = 10, Wiek = 45, StopienNaukowy = "Doktor", Stanowisko = "Kierownik Projektu", WynagrodzenieMiesieczne = 8000, IloscUrlopuDoPobrania = 25 },
+             new Pracownik { Imie = "Marek", Nazwisko = "Wiśniewski", IloscLatPrzepracowanych = 3, Wiek = 30, StopienNaukowy = "Inżynier", Stanowisko = "Programista", WynagrodzenieMiesieczne = 6000, IloscUrlopuDoPobrania = 18 },
+             new Pracownik { Imie = "Katarzyna", Nazwisko = "Kowalczyk", IloscLatPrzepracowanych = 8, Wiek = 40, StopienNaukowy = "Magister", Stanowisko = "Specjalista ds. HR", WynagrodzenieMiesieczne = 7000, IloscUrlopuDoPobrania = 22 },
+             new Pracownik { Imie = "Piotr", Nazwisko = "Lewandowski", IloscLatPrzepracowanych = 15, Wiek = 50, StopienNaukowy = "Profesor", Stanowisko = "Dyrektor", WynagrodzenieMiesieczne = 10000, IloscUrlopuDoPobrania = 30 },
+             new Pracownik { Imie = "Ewa", Nazwisko = "Zając", IloscLatPrzepracowanych = 7, Wiek = 38, StopienNaukowy = "Doktor", Stanowisko = "Kierownik Projektu", WynagrodzenieMiesieczne = 8500, IloscUrlopuDoPobrania = 24 },
+             new Pracownik { Imie = "Tomasz", Nazwisko = "Dąbrowski", IloscLatPrzepracowanych = 12, Wiek = 42, StopienNaukowy = "Inżynier", Stanowisko = "Programista", WynagrodzenieMiesieczne = 6200, IloscUrlopuDoPobrania = 21 },
+             new Pracownik { Imie = "Karolina", Nazwisko = "Szymańska", IloscLatPrzepracowanych = 6, Wiek = 33, StopienNaukowy = "Magister", Stanowisko = "Specjalista ds. HR", WynagrodzenieMiesieczne = 7200, IloscUrlopuDoPobrania = 23 },
+             new Pracownik { Imie = "Marcin", Nazwisko = "Wójcik", IloscLatPrzepracowanych = 4, Wiek = 28, StopienNaukowy = "Brak", Stanowisko = "Asystent", WynagrodzenieMiesieczne = 4500, IloscUrlopuDoPobrania = 19 },
+             new Pracownik { Imie = "Magdalena", Nazwisko = "Kamińska", IloscLatPrzepracowanych = 9, Wiek = 48, StopienNaukowy = "Magister", Stanowisko = "Kierownik Zespołu", WynagrodzenieMiesieczne = 7800, IloscUrlopuDoPobrania = 26 },
+             new Pracownik { Imie = "Łukasz", Nazwisko = "Woźniak", IloscLatPrzepracowanych = 11, Wiek = 44, StopienNaukowy = "Doktor", Stanowisko = "Starszy Analityk", WynagrodzenieMiesieczne = 8700, IloscUrlopuDoPobrania = 27 },
+             new Pracownik { Imie = "Natalia", Nazwisko = "Kowalczyk", IloscLatPrzepracowanych = 2, Wiek = 26, StopienNaukowy = "Brak", Stanowisko = "Asystent", WynagrodzenieMiesieczne = 4000, IloscUrlopuDoPobrania = 17 },
+             new Pracownik { Imie = "Michał", Nazwisko = "Zieliński", IloscLatPrzepracowanych = 16, Wiek = 52, StopienNaukowy = "Profesor", Stanowisko = "Dyrektor Naczelny", WynagrodzenieMiesieczne = 11000, IloscUrlopuDoPobrania = 32 },
+             new Pracownik { Imie = "Monika", Nazwisko = "Szymańska", IloscLatPrzepracowanych = 3, Wiek = 31, StopienNaukowy = "Magister", Stanowisko = "Specjalista ds. HR", WynagrodzenieMiesieczne = 6700, IloscUrlopuDoPobrania = 20 },
+             new Pracownik { Imie = "Tomasz", Nazwisko = "Woźniak", IloscLatPrzepracowanych = 7, Wiek = 36, StopienNaukowy = "Inżynier", Stanowisko = "Programista", WynagrodzenieMiesieczne = 5900, IloscUrlopuDoPobrania = 25 },
+             new Pracownik { Imie = "Kinga", Nazwisko = "Wójcik", IloscLatPrzepracowanych = 5, Wiek = 34, StopienNaukowy = "Magister", Stanowisko = "Analityk", WynagrodzenieMiesieczne = 5200, IloscUrlopuDoPobrania = 21 },
+             new Pracownik { Imie = "Krzysztof", Nazwisko = "Piotrowski", IloscLatPrzepracowanych = 14, Wiek = 49, StopienNaukowy = "Doktor", Stanowisko = "Kierownik Projektu", WynagrodzenieMiesieczne = 9300, IloscUrlopuDoPobrania = 28 },
+             new Pracownik { Imie = "Alicja", Nazwisko = "Kowalczyk", IloscLatPrzepracowanych = 6, Wiek = 32, StopienNaukowy = "Inżynier", Stanowisko = "Programista", WynagrodzenieMiesieczne = 6400, IloscUrlopuDoPobrania = 22 },
+             new Pracownik { Imie = "Mariusz", Nazwisko = "Lewandowski", IloscLatPrzepracowanych = 13, Wiek = 47, StopienNaukowy = "Magister", Stanowisko = "Specjalista ds. HR", WynagrodzenieMiesieczne = 8200, IloscUrlopuDoPobrania = 27 },
+             new Pracownik { Imie = "Agnieszka", Nazwisko = "Nowak", IloscLatPrzepracowanych = 4, Wiek = 29, StopienNaukowy = "Brak", Stanowisko = "Asystent", WynagrodzenieMiesieczne = 4700, IloscUrlopuDoPobrania = 19 },
+             new Pracownik { Imie = "Artur", Nazwisko = "Krawczyk", IloscLatPrzepracowanych = 11, Wiek = 46, StopienNaukowy = "Doktor", Stanowisko = "Dyrektor", WynagrodzenieMiesieczne = 9700, IloscUrlopuDoPobrania = 29 },
+             new Pracownik { Imie = "Ewelina", Nazwisko = "Mazur", IloscLatPrzepracowanych = 8, Wiek = 41, StopienNaukowy = "Magister", Stanowisko = "Kierownik Zespołu", WynagrodzenieMiesieczne = 7500, IloscUrlopuDoPobrania = 24 },
+             new Pracownik { Imie = "Robert", Nazwisko = "Jankowski", IloscLatPrzepracowanych = 2, Wiek = 27, StopienNaukowy = "Brak", Stanowisko = "Asystent", WynagrodzenieMiesieczne = 4300, IloscUrlopuDoPobrania = 18 },
+             new Pracownik { Imie = "Aleksandra", Nazwisko = "Wojciechowska", IloscLatPrzepracowanych = 9, Wiek = 43, StopienNaukowy = "Doktor", Stanowisko = "Starszy Analityk", WynagrodzenieMiesieczne = 8900, IloscUrlopuDoPobrania = 28 },
+             new Pracownik { Imie = "Mateusz", Nazwisko = "Kaczmarek", IloscLatPrzepracowanych = 14, Wiek = 51, StopienNaukowy = "Profesor", Stanowisko = "Dyrektor Naczelny", WynagrodzenieMiesieczne = 12000, IloscUrlopuDoPobrania = 33 },
+             new Pracownik { Imie = "Patrycja", Nazwisko = "Zając", IloscLatPrzepracowanych = 3, Wiek = 30, StopienNaukowy = "Magister", Stanowisko = "Specjalista ds. HR", WynagrodzenieMiesieczne = 6700, IloscUrlopuDoPobrania = 20 },
+             new Pracownik { Imie = "Adam", Nazwisko = "Kowalski", IloscLatPrzepracowanych = 6, Wiek = 34, StopienNaukowy = "Inżynier", Stanowisko = "Programista", WynagrodzenieMiesieczne = 6200, IloscUrlopuDoPobrania = 23 },
+             new Pracownik { Imie = "Sylwia", Nazwisko = "Dąbrowska", IloscLatPrzepracowanych = 12, Wiek = 48, StopienNaukowy = "Doktor", Stanowisko = "Kierownik Projektu", WynagrodzenieMiesieczne = 9100, IloscUrlopuDoPobrania = 27 },
+             new Pracownik { Imie = "Kamil", Nazwisko = "Mazur", IloscLatPrzepracowanych = 5, Wiek = 32, StopienNaukowy = "Magister", Stanowisko = "Analityk", WynagrodzenieMiesieczne = 5300, IloscUrlopuDoPobrania = 22 },
+             new Pracownik { Imie = "Klaudia", Nazwisko = "Witkowska", IloscLatPrzepracowanych = 9, Wiek = 40, StopienNaukowy = "Inżynier", Stanowisko = "Programista", WynagrodzenieMiesieczne = 6500, IloscUrlopuDoPobrania = 25 },
+             new Pracownik { Imie = "Rafał", Nazwisko = "Kaczmarek", IloscLatPrzepracowanych = 16, Wiek = 53, StopienNaukowy = "Profesor", Stanowisko = "Dyrektor Naczelny", WynagrodzenieMiesieczne = 11500, IloscUrlopuDoPobrania = 31 },
+             new Pracownik { Imie = "Marta", Nazwisko = "Jankowska", IloscLatPrzepracowanych = 4, Wiek = 31, StopienNaukowy = "Brak", Stanowisko = "Asystent", WynagrodzenieMiesieczne = 4800, IloscUrlopuDoPobrania = 20 },
+             new Pracownik { Imie = "Bartosz", Nazwisko = "Wojciechowski", IloscLatPrzepracowanych = 10, Wiek = 45, StopienNaukowy = "Doktor", Stanowisko = "Dyrektor", WynagrodzenieMiesieczne = 8900, IloscUrlopuDoPobrania = 28 },
+             new Pracownik { Imie = "Dominika", Nazwisko = "Krawczyk", IloscLatPrzepracowanych = 7, Wiek = 35, StopienNaukowy = "Magister", Stanowisko = "Kierownik Zespołu", WynagrodzenieMiesieczne = 7000, IloscUrlopuDoPobrania = 24 },
+             new Pracownik { Imie = "Kacper", Nazwisko = "Witkowski", IloscLatPrzepracowanych = 13, Wiek = 49, StopienNaukowy = "Doktor", Stanowisko = "Kierownik Projektu", WynagrodzenieMiesieczne = 9200, IloscUrlopuDoPobrania = 29 },
+             new Pracownik { Imie = "Wiktoria", Nazwisko = "Lewandowska", IloscLatPrzepracowanych = 8, Wiek = 42, StopienNaukowy = "Magister", Stanowisko = "Specjalista ds. HR", WynagrodzenieMiesieczne = 7600, IloscUrlopuDoPobrania = 25 },
+             new Pracownik { Imie = "Radosław", Nazwisko = "Mazurek", IloscLatPrzepracowanych = 3, Wiek = 29, StopienNaukowy = "Brak", Stanowisko = "Asystent", WynagrodzenieMiesieczne = 4600, IloscUrlopuDoPobrania = 19 },
+             new Pracownik { Imie = "Weronika", Nazwisko = "Kaczmarek", IloscLatPrzepracowanych = 6, Wiek = 33, StopienNaukowy = "Inżynier", Stanowisko = "Programista", WynagrodzenieMiesieczne = 6300, IloscUrlopuDoPobrania = 23 },
+             new Pracownik { Imie = "Mikołaj", Nazwisko = "Witkowski", IloscLatPrzepracowanych = 11, Wiek = 47, StopienNaukowy = "Doktor", Stanowisko = "Dyrektor", WynagrodzenieMiesieczne = 9600, IloscUrlopuDoPobrania = 30 },
+             new Pracownik { Imie = "Agata", Nazwisko = "Kaczmarek", IloscLatPrzepracowanych = 5, Wiek = 31, StopienNaukowy = "Magister", Stanowisko = "Analityk", WynagrodzenieMiesieczne = 5400, IloscUrlopuDoPobrania = 20 },
+             new Pracownik { Imie = "Łukasz", Nazwisko = "Lewandowski", IloscLatPrzepracowanych = 10, Wiek = 46, StopienNaukowy = "Doktor", Stanowisko = "Kierownik Zespołu", WynagrodzenieMiesieczne = 8800, IloscUrlopuDoPobrania = 26 },
+             new Pracownik { Imie = "Natalia", Nazwisko = "Zając", IloscLatPrzepracowanych = 2, Wiek = 28, StopienNaukowy = "Brak", Stanowisko = "Asystent", WynagrodzenieMiesieczne = 4400, IloscUrlopuDoPobrania = 18 },
+             new Pracownik { Imie = "Marcin", Nazwisko = "Witkowski", IloscLatPrzepracowanych = 7, Wiek = 36, StopienNaukowy = "Inżynier", Stanowisko = "Programista", WynagrodzenieMiesieczne = 6000, IloscUrlopuDoPobrania = 24 },
+             new Pracownik { Imie = "Aleksandra", Nazwisko = "Szymańska", IloscLatPrzepracowanych = 14, Wiek = 50, StopienNaukowy = "Doktor", Stanowisko = "Starszy Analityk", WynagrodzenieMiesieczne = 9200, IloscUrlopuDoPobrania = 29 }
+         };
+            return pracownicy;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace CWzLinq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Film> filmy = Film.GenerujFilmy();
+
+            // Znajdź wszystkie filmy wydane po roku 2000.
+            var filmyPo2000 = filmy.Where(f => f.RokWydania > 2000);
+            Console.WriteLine("Filmy wydane po roku 2000:");
+            Film.WyswietlWyniki(filmyPo2000);
+
+            Console.WriteLine();
+
+            // Znajdź wszystkie filmy z gatunku "Dramat".
+            var filmyDramatyczne = filmy.Where(f => f.Gatunek == "Dramat");
+            Console.WriteLine("Filmy z gatunku 'Dramat':");
+            Film.WyswietlWyniki(filmyDramatyczne);
+
+            Console.WriteLine();
+
+            // Znajdź średnią ocenę wszystkich filmów.
+            var sredniaOcena = filmy.Average(f => f.Ocena);
+            Console.WriteLine($"Średnia ocena wszystkich filmów: {sredniaOcena}");
+
+            Console.WriteLine();
+
+            // Znajdź 3 najlepiej ocenione filmy.
+            var najlepszeFilmy = filmy.OrderByDescending(f => f.Ocena).Take(3);
+            Console.WriteLine("Najlepiej ocenione filmy:");
+            Film.WyswietlWyniki(najlepszeFilmy);
+
+            Console.WriteLine();
+
+            // Posortuj filmy według roku wydania.
+            var posortowaneFilmy = filmy.OrderBy(f => f.RokWydania);
+            Console.WriteLine("Filmy posortowane według roku wydania:");
+            Film.WyswietlWyniki(posortowaneFilmy);
+
+            Console.WriteLine();
+
+            // Znajdź najstarszy film w bazie.
+            var najstarszyFilm = filmy.OrderBy(f => f.RokWydania).First();
+            Console.WriteLine($"Najstarszy film w bazie: {najstarszyFilm.Tytul}, Rok wydania: {najstarszyFilm.RokWydania}");
+
+            // Znajdź najnowszy film w bazie.
+            var najnowszyFilm = filmy.OrderByDescending(f => f.RokWydania).First();
+            Console.WriteLine($"Najnowszy film w bazie: {najnowszyFilm.Tytul}, Rok wydania: {najnowszyFilm.RokWydania}");
+
+            Console.WriteLine();
+
+            // Znajdź wszystkie unikalne gatunki filmowe obecne w bazie.
+            var unikalneGatunki = filmy.Select(f => f.Gatunek).Distinct();
+            Console.WriteLine("Unikalne gatunki filmowe:");
+            foreach (var gatunek in unikalneGatunki)
+            {
+                Console.WriteLine(gatunek);
+            }
+
+            Thread.Sleep(30000); // Dodany wątek oczekiwania na zakończenie, aby program nie zamykał się natychmiastowo
+        }
+    }
+
+    public class Film
+    {
+        public string Tytul { get; set; }
+        public int RokWydania { get; set; }
+        public string Gatunek { get; set; }
+        public double Ocena { get; set; }
+
+        public Film(string tytul, int rokWydania, string gatunek, double ocena)
+        {
+            Tytul = tytul;
+            RokWydania = rokWydania;
+            Gatunek = gatunek;
+            Ocena = ocena;
+        }
+
+        public static List<Film> GenerujFilmy()
+        {
+            List<Film> filmy = new List<Film>
+            {
+                new Film("The Shawshank Redemption", 1994, "Dramat", 9.3),
+        new Film("The Godfather", 1972, "Dramat", 9.2),
+        new Film("The Dark Knight", 2008, "Akcja", 9.0),
+        new Film("Schindler's List", 1993, "Biograficzny", 8.9),
+        new Film("The Lord of the Rings: The Return of the King", 2003, "Fantasy", 8.9),
+        new Film("Pulp Fiction", 1994, "Kryminał", 8.9),
+        new Film("The Lord of the Rings: The Fellowship of the Ring", 2001, "Fantasy", 8.8),
+        new Film("Forrest Gump", 1994, "Dramat", 8.8),
+        new Film("Fight Club", 1999, "Dramat", 8.8),
+        new Film("Inception", 2010, "Sci-Fi", 8.7),
+        new Film("The Lord of the Rings: The Two Towers", 2002, "Fantasy", 8.7),
+        new Film("Star Wars: Episode V - The Empire Strikes Back", 1980, "Sci-Fi", 8.7),
+        new Film("The Matrix", 1999, "Sci-Fi", 8.7),
+        new Film("Goodfellas", 1990, "Biograficzny", 8.7),
+        new Film("One Flew Over the Cuckoo's Nest", 1975, "Dramat", 8.7),
+        new Film("Se7en", 1995, "Kryminał", 8.6),
+        new Film("The Silence of the Lambs", 1991, "Kryminał", 8.6),
+        new Film("City of God", 2002, "Dramat", 8.6),
+        new Film("The Green Mile", 1999, "Dramat", 8.6),
+        new Film("The Usual Suspects", 1995, "Kryminał", 8.5),
+        new Film("Léon: The Professional", 1994, "Akcja", 8.5),
+        new Film("Saving Private Ryan", 1998, "Wojenny", 8.5),
+        new Film("Interstellar", 2014, "Sci-Fi", 8.5),
+        new Film("The Pianist", 2002, "Biograficzny", 8.5),
+        new Film("Gladiator", 2000, "Akcja", 8.5),
+        new Film("American History X", 1998, "Dramat", 8.5),
+        new Film("The Departed", 2006, "Kryminał", 8.5),
+        new Film("Whiplash", 2014, "Dramat", 8.5),
+        new Film("The Lion King", 1994, "Animacja", 8.5),
+        new Film("The Prestige", 2006, "Mystery", 8.5),
+        new Film("The Intouchables", 2011, "Biograficzny", 8.5),
+        new Film("The Godfather: Part II", 1974, "Dramat", 8.5),
+        new Film("The Sixth Sense", 1999, "Thriller", 8.5),
+        new Film("The Departed", 2006, "Kryminał", 8.5),
+        new Film("The Shining", 1980, "Horror", 8.4),
+        new Film("The Truman Show", 1998, "Dramat", 8.4),
+        new Film("The Dark Knight Rises", 2012, "Akcja", 8.4),
+        new Film("The Lives of Others", 2006, "Dramat", 8.4),
+        new Film("The Help", 2011, "Dramat", 8.4),
+        new Film("The Terminator", 1984, "Sci-Fi", 8.4),
+        new Film("The Matrix", 1999, "Sci-Fi", 8.4),
+        new Film("The Silence of the Lambs", 1991, "Kryminał", 8.4)
+            };
+
+            return filmy;
+        }
+
+        public static void WyswietlWyniki(IEnumerable<Film> wyniki)
+        {
+            foreach (var film in wyniki)
+            {
+                Console.WriteLine($"Tytuł: {film.Tytul}, Rok wydania: {film.RokWydania}, Gatunek: {film.Gatunek}, Ocena: {film.Ocena}");
+            }
+        }
+    }
+}
